96190130e4818992e369ff4687e968ce04d436ad
deshollow <555demin@gmail.com>
2024-03-09 14:33:22 +0300

    Initial Commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by deshollow on 08.03.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
